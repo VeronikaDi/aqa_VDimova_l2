adc8f7e707bf5dac475d73e2b3745cff8a9a6bc9
VeronikaDi <veronika.dikaieva@gmail.com>
2024-09-08 20:06:25 +0300

    Done: Homework 16 in Task_1_HW_16 and Task_2_HW_16

---
+++ lesson_16/Task_2_HW_16/class_rectangle.py
@@ -0,0 +1,22 @@
+from typing import override
+
+from lesson_16.Task_2_HW_16.class_figure import Figure
+
+
+class Rectangle(Figure):
+
+    __rectangle_a: int
+    __rectangle_b: int
+
+    def __init__(self, rectangle_a: int, rectangle_b: int):
+        self.__rectangle_a = rectangle_a
+        self.__rectangle_b = rectangle_b
+
+    @override
+    def calculate_perimetr(self) -> int:
+        return (self.__rectangle_a * 2) + (self.__rectangle_b * 2)
+
+    @override
+    def calculate_square(self) -> int:
+        return self.__rectangle_a * self.__rectangle_b
+    
