ccabd56d9838301a9ad155a72b42b41aaca3fa39
VeronikaDi <veronika.dikaieva@gmail.com>
2024-10-24 23:06:31 +0300

    Delete lesson_30 directory

--- lesson_30/abstract_page.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC
-from selenium.webdriver.ie.webdriver import WebDriver
-
-
-class AbstractPage(ABC):
-    _driver: WebDriver
-
-    def __init__(self, driver: WebDriver) -> None:
-        self._driver = driver
